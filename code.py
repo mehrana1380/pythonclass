59f7e82598867751e49f2ae31030692d90d3ad77
mehrana1380 <84845533+mehrana1380@users.noreply.github.com>
2021-06-01 00:24:50 -0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+print ("adad vared kon: ")
+first_number = input()
+first_number_float = float(first_number)
+
+print("adad dovom ra vared kon")
+second_number = input()
+second_number_float = float(second_number)
+
+result= second_number_float + first_number_float
+
+print("natije: ")
+print(result)
